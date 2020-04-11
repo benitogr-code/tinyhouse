52a2af081cd52c54890b80eb20325d5a6d241404
BenitoGR <benito.gangosorodriguez@gmail.com>
2020-04-11 16:12:30 +0200

    Add grapql schema language to define types

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,11 @@
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
-import { schema } from "./graphql";
+import { typeDefs, resolvers } from "./graphql";
 
 const port = 9000;
 const app = express();
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app, path: "/api" });
 
 app.listen(port, () => {
