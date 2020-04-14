522f53360b56275c2b54e71a38b40f977c9e6345
BenitoGR <benito.gangosorodriguez@gmail.com>
2020-04-14 16:32:45 +0200

    Add functionality to fetch data using server api

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,9 +1,37 @@
 import React from "react";
+import { server } from "../../lib/api"
+
+const GraphQLQuery = {
+  Listings: `
+    query Listings {
+      listings {
+        id
+        title
+        image
+        address
+        price
+        numOfGuests
+        numOfBeds
+        rating
+      }
+    }
+  `,
+};
 
 interface Props {
   title: string;
 }
 
 export const Listings = (props: Props) => {
-  return <h2>{props.title}</h2>;
+  const fetchListings = async () => {
+    const { data } = await server.fetch({ query: GraphQLQuery.Listings });
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <button onClick={fetchListings}>Query Listings!</button>
+    </div>
+  );
 };
