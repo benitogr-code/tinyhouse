8f3c110d403b17a160a5624dca3341e635d5dcc1
BenitoGR <benito.gangosorodriguez@gmail.com>
2020-04-27 17:52:58 +0200

    Add UserBookings and UserListings to User section

--- client/src/lib/utils/index.ts
+++ client/src/lib/utils/index.ts
@@ -1,5 +1,7 @@
 import { message, notification } from "antd";
 
+export const iconColor = "#1890ff";
+
 export const displaySuccessNotification = (message: string, description?: string) => {
   return notification.success({
     message,
@@ -14,3 +16,9 @@ export const displaySuccessNotification = (message: string, description?: string
 export const displayErrorMessage = (error: string) => {
   return message.error(error);
 };
+
+export const formatListingPrice = (price: number, round: boolean = true) => {
+  const formatted = round ? Math.round(price/100) : (price/100);
+
+  return `$${formatted}`;
+}
--- client/src/sections/User/index.tsx
+++ client/src/sections/User/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { Layout, Col, Row } from "antd";
 import { useQuery } from "react-apollo";
-import { UserProfile } from "./components";
+import { UserBookings, UserListings, UserProfile } from "./components";
 import { User as UserQuery } from "../../lib/graphql/queries";
 import { User as UserData, UserVariables } from "../../lib/graphql/queries/__generated__/User";
 import { ErrorBanner, PageSkeleton } from "../../lib/components";
@@ -17,9 +17,16 @@ interface MatchParams {
 }
 
 export const User = ({ viewer, match }: Props & RouteComponentProps<MatchParams>) => {
+  const pageLimit = 4;
+  const [listingsPage, setListingsPage] = useState(1);
+  const [bookingsPage, setBookingsPage] = useState(1);
+
   const { data, loading, error } = useQuery<UserData, UserVariables>(UserQuery, {
     variables: {
-      id: match.params.id
+      id: match.params.id,
+      bookingsPage,
+      listingsPage,
+      limit: pageLimit
     }
   });
 
@@ -44,10 +51,22 @@ export const User = ({ viewer, match }: Props & RouteComponentProps<MatchParams>
   const viewerIsUser = user ? user.id === viewer.id : false;
   const userProfileElement = user ? <UserProfile user={user} viewerIsUser={viewerIsUser} /> : null;
 
+  const userListingsElement = user && user.listings
+    ? <UserListings listings={user.listings} page={listingsPage} limit={pageLimit} setPage={setListingsPage} />
+    : null;
+
+  const userBookingsElement = user && user.bookings
+    ? <UserBookings bookings={user.bookings} page={bookingsPage} limit={pageLimit} setPage={setBookingsPage} />
+    : null;
+
   return (
     <Layout.Content className="user">
       <Row gutter={12} type="flex" justify="space-between">
         <Col xs={24}>{userProfileElement}</Col>
+        <Col xs={24}>
+          {userListingsElement}
+          {userBookingsElement}
+        </Col>
       </Row>
     </Layout.Content>
   );
