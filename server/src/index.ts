7eca8903bda5b972a114aba9d38759c2c3d23897
BenitoGR <benito.gangosorodriguez@gmail.com>
2020-05-31 08:36:33 +0200

    Server app provides access to client frontend in a production environment

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,24 @@
 import { ApolloServer } from "apollo-server-express";
 import bodyParser from "body-parser";
+import compression from "compression";
 import cookieParser from "cookie-parser";
 import express, { Application } from "express";
 import { connectDatabase } from "./database";
 import { typeDefs, resolvers } from "./graphql";
 
-const { APP_SECRET, PORT } = process.env;
+const { APP_SECRET, PORT, NODE_ENV } = process.env;
 
 async function start(app: Application, port: number) {
   const db = await connectDatabase();
 
   app.use(bodyParser.json({ limit: "2mb" }));
   app.use(cookieParser(APP_SECRET));
+
+  if (NODE_ENV === "production") {
+    app.use(compression());
+    app.use(express.static(`${__dirname}/public`));
+    app.get("/*", (_req, res) => res.sendFile(`${__dirname}/public/index.html`));
+  }
 
   const server = new ApolloServer({
     typeDefs,
@@ -21,10 +28,7 @@ async function start(app: Application, port: number) {
   server.applyMiddleware({ app, path: "/api" });
 
   app.listen(port, async () => {
-    console.log(`[app] server running http://localhost:${port}`)
-
-    //const listings = await db.listings.find({}).toArray();
-    //console.log(listings);
+    console.log(`[app] server running and listening at port: ${port}`)
   });
 }
 
