80c4ce4ac3929c9d3eb0602a33e21fd7df5c59aa
BenitoGR <benito.gangosorodriguez@gmail.com>
2020-05-31 11:51:27 +0200

    Implement NotFound component

--- client/src/sections/NotFound/index.tsx
+++ client/src/sections/NotFound/index.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import { Empty, Layout, Typography } from "antd";
 
 export const NotFound = () => {
-  return <h2>NotFound</h2>;
+  return (
+    <Layout.Content className="not-found">
+      <Empty
+        description={
+          <Fragment>
+            <Typography.Text className="not-found__description-title">
+              Uh oh! Something went wrong :(
+            </Typography.Text>
+            <Typography.Text className="not-found__description-subtitle">
+              The page you're looking for can't be found
+            </Typography.Text>
+          </Fragment>
+        }
+      />
+      <Link to="/" className="not-found__cta ant-btn ant-btn-primary ant-btn-lg">
+        Go to Home
+      </Link>
+    </Layout.Content>
+  );
 };
